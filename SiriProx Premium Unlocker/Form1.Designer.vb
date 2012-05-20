b7e6b2ded3388d0e36f554e41b79fc5d487e9a28
sunra1n <sun.ra1n@me.com>
2012-05-20 17:45:27 +0200

    ...

---
+++ SiriProx Premium Unlocker/Form1.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.TextBox1.Location = New System.Drawing.Point(17, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(349, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "Loading ..."
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.Location = New System.Drawing.Point(17, 66)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(349, 20)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(17, 105)
+        Me.ProgressBar1.MarqueeAnimationSpeed = 20
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(349, 23)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(17, 89)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(349, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Please connect your idevice ..."
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(44, 134)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(294, 43)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Buy now"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(-29, 180)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(952, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = resources.GetString("Label1.Text")
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Silver
+        Me.Label3.Location = New System.Drawing.Point(16, 193)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(141, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "© 2012 @ iRa1nDev Team "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(17, 50)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "UDID :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(17, 11)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(43, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "HWID :"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI Light", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(17, 222)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(362, 77)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "You'll have to send us your HWID and UDID" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "in the paypal mail or we can't activat" & _
+    "e your" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "iDevice for SiriProx."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(390, 309)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "SiriProx Premium Unlocker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+
+End Class
---
+++ SiriProx Premium Unlocker/Form1.vb
@@ -0,0 +1,89 @@
+﻿Option Strict On
+Imports System.Management
+Imports System
+Imports MobileDevice
+Public Class Form1
+
+    Dim iPhonev As String
+    Dim Name As String
+    Dim Abfrage As New SelectQuery("win32_processor")
+    Dim Suche As New ManagementObjectSearcher(Abfrage)
+    Dim MO As ManagementObject
+    Dim iphone As New MobileDevice.iPhone
+    Friend Function HWID() As String
+        For Each MO In Suche.Get()
+            Name = CStr(MO("processorID"))
+        Next
+        Return Name
+         BackgroundWorker1.CancelAsync()
+    End Function
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        CheckForIllegalCrossThreadCalls = False
+        BackgroundWorker1.RunWorkerAsync()
+
+
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As System.Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Do Until iphone.IsConnected = True
+            Application.DoEvents()
+        Loop
+
+        If iphone.DeviceProductType = "iPod3,1" Then
+            iPhonev = "iPod Touch 3G"
+
+        ElseIf iphone.DeviceProductType = "iPod4,1" Then
+            iPhonev = "iPod Touch 4"
+
+        ElseIf iphone.DeviceProductType = "iPod2,1" Then
+            iPhonev = "iPod Touch 2G"
+
+        ElseIf iphone.DeviceProductType = "iPod1,1" Then
+            iPhonev = "iPod touch 1G"
+
+        ElseIf iphone.DeviceProductType = "AppleTV2,1" Then
+            iPhonev = "Apple TV 2G"
+
+        ElseIf iphone.DeviceProductType = "iPad1,1" Then
+            iPhonev = "iPad 1G"
+
+        ElseIf iphone.DeviceProductType = "iPad2,1" Then
+            iPhonev = "iPad 2G"
+
+        ElseIf iphone.DeviceProductType = "iPad2,2" Then
+            iPhonev = "iPad 2G"
+
+        ElseIf iphone.DeviceProductType = "iPad2,3" Then
+            iPhonev = "iPad 2G"
+
+        ElseIf iphone.DeviceProductType = "iPhone1,1" Then
+            iPhonev = "iPhone 2G"
+
+        ElseIf iphone.DeviceProductType = "iPhone1,2" Then
+            iPhonev = "iPhone 3G"
+
+        ElseIf iphone.DeviceProductType = "iPhone2,1" Then
+            iPhonev = "iPhone 3GS"
+
+        ElseIf iphone.DeviceProductType = "iPhone3,3" Then
+            iPhonev = "iPhone 4"
+
+        ElseIf iphone.DeviceProductType = "iPhone3,1" Then
+            iPhonev = "iPhone 4"
+
+        ElseIf iphone.DeviceProductType = "iPhone4,1" Then
+            iPhonev = "iPhone 4S"
+
+        End If
+    
+        Label2.Text = "  " & iPhonev & " connected"
+        TextBox2.Text = iphone.DeviceId
+        TextBox1.Text = HWID()
+
+    End Sub
+
+
+    Private Sub Button1_Click_1(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Process.Start("change that")
+    End Sub
+End Class
